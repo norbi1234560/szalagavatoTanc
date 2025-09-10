3ac4d843254e7be5cc15a0681615fa225797ad21
kardos-zoltan <kardos.zoltan-2021@keri.mako.hu>
2025-09-10 13:52:55 +0200

    get logged user php addolva

---
+++ php/getLoggedUser.php
@@ -0,0 +1,19 @@
+<?php
+require_once("../../common/php/environment.php");
+
+if ($_COOKIE["email"]) Util::setResponse();
+
+$args = Util::getArgs();
+
+$db = new Database();
+
+$query = "SELECT `id`, `name`, `email` FROM `users` WHERE `email` = :email LIMIT 1";
+$result = $db->execute($query, $args["email"]);
+
+if (count($result) === 0) {
+    Util::setResponse(false);
+}
+
+$db = null;
+
+Util::setResponse($result[0]);
