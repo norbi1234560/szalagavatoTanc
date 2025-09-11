a6fb3514f04de6e0c655cd2f657dc87860b74b96
BertaBarnabas2006 <bertabarnabas2006@gmail.com>
2025-09-11 13:40:12 +0200

    removePairsPhp fix

--- php/removePairs.php
+++ php/removePairs.php
@@ -5,8 +5,9 @@ require_once("../../common/php/environment.php");
 $db = new Database();
 
 $query = "DELETE FROM `pairs`";
+$result = $db->execute($query);
 $query = "UPDATE `students` SET `taken` = 0";
+$result = $db->execute($query);
 
 $db = null;
-
 Util::setResponse();
