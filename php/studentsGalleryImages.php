4de7dcddf7eca67fa1ad29fe9b6eeffd6476dbb4
guvat-bence <guvat.bence-2021@keri.mako.hu>
2025-09-23 11:09:09 +0200

    studentsGalleryImages.php

---
+++ php/studentsGalleryImages.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once("../../common/php/environment.php");
+
+
+$assets = "./assets/pics";
+
+$files = scandir($assets);
+
+$galleryImages = array();
+
+foreach ($files as $classes) {
+
+  if (str_contains($classes, "13")) {
+    $commondir = "./assets/pics/$classes/common";
+    $commonFolder = scandir($commondir);
+    foreach ($commonFolder as $common) {
+      if ($common != "." && $common != "..") {
+        $galleryImages[] = "$commondir/$common";
+      }
+    }
+  }
+}
+
+echo $galleryImages;
